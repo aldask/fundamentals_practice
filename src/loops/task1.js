7c8ca07b8d22bd07ddc9006f168ed5638fa7e813
Aldas <80693733+aldask@users.noreply.github.com>
2023-02-16 22:44:27 +0200

    loops

---
+++ src/loops/task1.js
@@ -0,0 +1,7 @@
+for(let i = 10; i <= 30; i++){
+    if(i % 2 == 0){
+        console.log(`Even number: ${i}`);
+    } else {
+        console.log(`Odd number: ${i}`);
+    }
+}
---
+++ src/loops/task2.js
@@ -0,0 +1,7 @@
+const title = "theBigName";
+let splitted = [];
+for (let i = 0; i < title.length; i++){
+    let splitted = title[i];
+    console.log(splitted);
+
+}
