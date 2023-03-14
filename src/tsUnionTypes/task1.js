c14c20457041173b7980690d64543656ca110784
Aldas <80693733+aldask@users.noreply.github.com>
2023-03-14 20:03:50 +0200

    union types

---
+++ src/tsUnionTypes/task1.js
@@ -0,0 +1,15 @@
+/*
+Create a file task1.js in src/tsUnionTypes. Put the solution in this file of the task bellow
+
+Create let variable without initial value. Choose necessary data types to be able implement next task items
+
+Assign string value devMentor and print variable to the console
+
+Assign a number 10 and print variable to the console
+*/
+/// TASK 1
+var abrakadabra;
+abrakadabra = "devMentor";
+console.log(abrakadabra);
+abrakadabra = 9999;
+console.log(abrakadabra);
---
+++ src/tsUnionTypes/task2.ts
@@ -0,0 +1,30 @@
+/*
+Create a file task2.js in src/tsUnionTypes. Put the solution in this file of the task bellow
+
+Create a function with optional string type argument
+
+If an argument was passed - function returns a number of uppercase letters in that string
+
+If an argument wasn’t passed - function returns null value
+
+E.g. calling function: countUppercase('devMentor'); The result should be: 1
+*/
+
+
+const counter = (txt?: string): number | null => {
+    if (txt) {
+        let count = 0;
+        for (let i = 0; i < txt.length; i++) {
+            if (txt[i] === txt[i].toUpperCase()) {
+                count++;
+            }
+        }
+        return count;
+    } else {
+        return null;
+    }
+}
+
+console.log(counter());
+console.log(counter("tEst"));
+console.log(counter("tESt"));
